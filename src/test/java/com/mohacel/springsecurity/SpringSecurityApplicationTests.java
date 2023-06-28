1198969d3b0e00175508b037394593fa64acf901
mohacelhosen <mohacel.hosen22@gmail.com>
2023-06-28 15:14:18 +0600

    Spring security 6

---
+++ src/test/java/com/mohacel/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohacel.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
