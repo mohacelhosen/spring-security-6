8c1fecc42ab54e75d0f5949de416330d20e07ca8
mohacelhosen <mohacel.hosen22@gmail.com>
2023-06-28 16:14:47 +0600

    REST controller class completed

--- src/main/java/com/mohacel/springsecurity/controller/UserRestController.java
+++ src/main/java/com/mohacel/springsecurity/controller/UserRestController.java
@@ -1,12 +1,20 @@
 package com.mohacel.springsecurity.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mohacel.springsecurity.dto.UserDto;
+import com.mohacel.springsecurity.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserRestController {
+    @Autowired
+    private IUserService service;
+
 
     @GetMapping("/welcome")
     public String welcome(){
@@ -17,5 +25,28 @@ public class UserRestController {
     public String test(){
         String msg = "Authentication Accepted ✅";
         return msg;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody UserDto user){
+        boolean register = service.register(user);
+        String message = null;
+        if (register==true){
+            message="Registration Successfully ✅";
+        }
+        message="Registration Fail ❌";
+        return  new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Integer userId){
+        UserDto userById = service.findUserById(userId);
+        return  new ResponseEntity<>(userById, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<UserDto>> allUser(){
+        List<UserDto> userDtoList = service.allUser();
+        return new ResponseEntity<>(userDtoList, HttpStatus.OK);
     }
 }
