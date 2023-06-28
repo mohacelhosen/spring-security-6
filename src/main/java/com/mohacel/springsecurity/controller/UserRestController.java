9683bd869adf75eee3fff26cc2fa38a664bf3b60
mohacelhosen <mohacel.hosen22@gmail.com>
2023-06-28 15:24:53 +0600

    Spring security dependecy added

---
+++ src/main/java/com/mohacel/springsecurity/controller/UserRestController.java
@@ -0,0 +1,21 @@
+package com.mohacel.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserRestController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome the world of Spring rest API";
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        String msg = "Authentication Accepted";
+        return msg;
+    }
+}
