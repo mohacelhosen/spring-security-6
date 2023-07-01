c59165cd911e21de3340edb54d68f0f3c601974c
mohacelhosen <mohacel.hosen22@gmail.com>
2023-07-01 15:16:31 +0600

    login method updated

--- src/main/java/com/mohacel/springsecurity/controller/LoginController.java
+++ src/main/java/com/mohacel/springsecurity/controller/LoginController.java
@@ -1,9 +1,14 @@
 package com.mohacel.springsecurity.controller;
 
 import com.mohacel.springsecurity.dto.LoginInfo;
+import com.mohacel.springsecurity.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,13 +16,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Controller
 public class LoginController {
     @Autowired
+    private JwtService jwtService;
+    @Autowired
     private AuthenticationManager authenticationManager;
 
     @PostMapping("/user/login")
-    public ResponseEntity<String> login(@RequestBody LoginInfo userLoginInfo){
-
-
-        return null;
+    public ResponseEntity<?> login(@RequestBody LoginInfo userLoginInfo){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userLoginInfo.getEmail(), userLoginInfo.getPassword()));
+        if (authentication.isAuthenticated()){
+            String token = jwtService.generateToken(userLoginInfo.getEmail());
+            return new ResponseEntity<>(token, HttpStatus.OK);
+        }
+        throw new BadCredentialsException("Invalid email or password!");
     }
-
 }
--- src/main/java/com/mohacel/springsecurity/service/JwtService.java
+++ src/main/java/com/mohacel/springsecurity/service/JwtService.java
@@ -1,27 +1,34 @@
 package com.mohacel.springsecurity.service;
 
+import com.mohacel.springsecurity.entity.UserEntity;
+import com.mohacel.springsecurity.repository.UserRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 @Service
 public class JwtService {
+    @Autowired
+    private UserRepository userRepository;
     private static final long EXPIRE_TIME = 1000*60*5;
     private final String SECRET_KEY="377529A78E6E4527AA41795AFE99D";
 
-    public String generateToken(String email, String userName, List<String> roles) {
+    public String generateToken(String email) {
+        Optional<UserEntity> user = userRepository.findUserEntityByEmail(email);
+
         Map<String, Object> claims = new HashMap<>();
-        claims.put("name", userName);
-        claims.put("role", roles);
+        if (user.isPresent()){
+            claims.put("name", user.get().getName());
+            claims.put("role", user.get().getRole());
+        }
         String token = createToken(claims, email);
         return token;
     }
@@ -40,4 +47,5 @@ public class JwtService {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
+
 }
